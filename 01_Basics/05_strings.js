63aecc381c6192b6e1a72a84442aa4d37eee4a4b
Maha Shahzadi <104192800+MahaAbdulMukhtar@users.noreply.github.com>
2026-06-28 07:01:28 +0000

    Strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
-const repoCount = 50
+const name = "maha"
+const repoCount = 10
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('maha-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,11 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   maha    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
+const url = "https://example.com/example%20xyz"
 
 console.log(url.replace('%20', '-'))
 
